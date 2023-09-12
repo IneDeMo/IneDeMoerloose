53e29943b92f2bfa6552cdabb03a96fd58792a6a
IneDeMo <143532606+IneDeMo@users.noreply.github.com>
2023-09-12 14:25:28 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <body class="home-background">
+              <div>
+                  <ul class="event-data">
+                      <li><a class="datatext" href="https://compagnieamai.com/tickets/comedy-open-mic-nl/#tribe-tickets" target="_blank">15/09 Amai Comedy Club - Gent</a></li>
+                      <li><a class="datatext" href="https://tinyurl.com/2vjspvph" target="_blank">25/09 G-huis - Melle</a></li>
+                      <li><a class="datatext" href="https://www.wicket.be/triporteur/comedy-the-brewery-open-mic1" target="_blank">07/10 Comedy at the Brewery - Roeselare</a></li>
+                      <li><a class="datatext" href="https://www.comedykelder.be/abonnement" target="_blank">19/03 Comedykelder - Deinze</a></li>
+                  </ul>
+                  <div class="data">
+                  </div>
+              </div>
+          </body>
+    </div>
+  );
+}
+
+export default App;
